236f45cce2778dee129a87e077ecfd7383c8eab1
JuanBravo <juanbravo@JuanBravos-MacBook-Pro.local>
2022-05-14 13:14:41 +0300

    Solve dependency issues and minor syntax problem

--- app/renderer/src/index.js
+++ app/renderer/src/index.js
@@ -1,7 +1,6 @@
 /* global document */
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
 const rootNode = document.getElementById('app');
-ReactDOM.render(<App />, rootNode);
+ReactDOM.render(App, rootNode);
